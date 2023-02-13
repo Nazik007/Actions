a77fade9d552ed83c35d1b2c8bfc09c844b6a0cc
Nazarii Soviak <natesoviak@gmail.com>
2023-02-13 17:51:42 +0200

    Test1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,5 +5,6 @@ public class main {
     public static void main(String[] args) {
         SumCalculator sumCalculator = new SumCalculator();
         System.out.println(sumCalculator.sum(4));
+        System.out.println(sumCalculator.sum(2));
     }
 }
